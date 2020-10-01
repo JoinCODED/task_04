7d6fb21e389a40c1e489ccc2694d1d64214916d6
Laksh8 <41758625+Laksh8@users.noreply.github.com>
2020-10-01 09:17:29 +0530

    Update views.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
+from django.views import View
 
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
-def restaurant_list(request):
-    context = {
-        "my_list":[
-            {
-                'name':'Subway',
-                'food_type':'Sandwiches.',
-            },
-            {
-                'name':'Steers',
-                'food_type':'Burgers, Fries, and Pasta.',
-            },
-            {
-                'name':'Backyard',
-                'food_type':'Been there, done that...',
-            },
-        ]
-    }
-    return render(request, 'list.html', context)
+class restaurant_list(View):    # Use class Based view 
+    def get(self,request):
+        context = {
+            "my_list":[
+                {
+                    'name':'Subway',
+                    'food_type':'Sandwiches.',
+                },
+                {
+                    'name':'Steers',
+                    'food_type':'Burgers, Fries, and Pasta.',
+                },
+                {
+                    'name':'Backyard',
+                    'food_type':'Been there, done that...',
+                },
+            ]
+        }
+        return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
