12deb89312fdb001b14dd44863cf35f72b1d62a5
Agus Mathews <agus@tentwentyfour.lu>
2020-10-02 16:32:20 +0200

    created model and objects

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    opening_time = models.DateTimeField(blank=True, null=True)
+    closing_time = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
